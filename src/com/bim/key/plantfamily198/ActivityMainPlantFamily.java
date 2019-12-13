01bd95607cde5bff3c5a2d5ee6ad8a2d371ab4cd
hongpingliang <hongpingliang@gmail.com>
2019-12-13 11:41:11 -0500

    init

---
+++ src/com/bim/key/plantfamily198/ActivityMainPlantFamily.java
@@ -0,0 +1,12 @@
+package com.bim.key.plantfamily198;
+
+import com.bim.key.ActivityMain;
+import com.bim.key.core.Setting;
+
+public class ActivityMainPlantFamily extends ActivityMain {
+	static {
+		URL_SCHEMA = "com.bim.key.plantfamily198";
+		Setting.IS_CHECK_ORIGINAL_IMAGE_EXIST = true;
+		Setting.Language_Other = true;
+	}
+}
